973fabfd447278bb4b8fccfe5346d778bf4ab439
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-12-18 21:38:33 +0530

    Demo SSG for dynamic routes

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -15,12 +15,24 @@ function MeetupDetails({ meetup }) {
   );
 }
 
-export async function getServerSideProps(context) {
-  const meetupId = context.params.meetupId;
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId; // provided by getStaticPaths
   const meetup = DUMMY_MEETUPS.find(({ id }) => id === meetupId);
 
   if (!meetup) return { notFound: true };
   return { props: { meetup: meetup } };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [
+      { params: { meetupId: "m1" } },
+      { params: { meetupId: "m2" } },
+      { params: { meetupId: "m3" } },
+    ],
+    fallback: false, // All pages built. Show 404 for other routes.
+    // fallback: true, // Not all pages built. Build and cache pages for unmentioned routes on request
+  };
 }
 
 export default MeetupDetails;
