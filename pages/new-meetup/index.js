23a9c01fd1b17093a81e4708038528fae2f57fb6
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-12-24 04:49:10 +0530

    Add HTTP POST code on form submission

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,22 @@
+import { useRouter } from "next/router";
+
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  const addMeetupHanlder = (enteredMeeupData) => {
-    console.log(enteredMeeupData);
+  const router = useRouter();
+
+  const addMeetupHanlder = async (enteredMeeupData) => {
+    // route - /api instead of www.somethingxyz.com, as the UI and web app server are the same.
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeeupData), // assume data is valid
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    // assume success, no message needed - just redirect
+    router.push("/");
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHanlder} />;
