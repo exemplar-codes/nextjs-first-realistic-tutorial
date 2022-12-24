e40ee3fcd3bffd6129c75a9484d5fffe4c147aed
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-12-25 02:00:00 +0530

    Demo Head component provided by Next.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 import { MongoClient } from "mongodb";
+
+import Head from "next/head";
+
 import MeetupList from "../components/meetups/MeetupList";
 
 function HomePage({ meetups }) {
-  return <MeetupList meetups={meetups} />;
+  return (
+    <>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={meetups} />
+    </>
+  );
 }
 
 export async function getStaticProps() {
